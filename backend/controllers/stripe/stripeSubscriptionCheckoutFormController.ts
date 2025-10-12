81cd828bd180c2df6f69e82f0478bc6cd890297a
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-10-12 16:14:34 -0400

    added comments

--- backend/controllers/stripe/stripeSubscriptionCheckoutFormController.ts
+++ backend/controllers/stripe/stripeSubscriptionCheckoutFormController.ts
@@ -7,11 +7,14 @@ import { LandlordModel } from "../../database/models/Landlord.model";
 dotenv.config();
 
 export const stripeSubscriptionCheckoutFormController = async (req: Request, res: Response) => {
+  // Pulling in access token from param
   const accessToken = req.params.id
 
+  // Declaring Stripe secret key and JWT token
   const STRIPE_TEST_SECRET_KEY = process.env.STRIPE_TEST_SECRET_KEY
   const JWT_ACCESS_TOKEN = process.env.JWT_ACCESS_TOKEN!
 
+  // This entire block is the subscription form using stripe to redirect to a new page
   try {
     const decoded = jwt.verify(accessToken, JWT_ACCESS_TOKEN)
 
@@ -19,6 +22,7 @@ export const stripeSubscriptionCheckoutFormController = async (req: Request, res
       return res.status(400).json({ message: "Something's wrong with your access token." })
     }
 
+    // Getting user landlord from database
     const landlord = await LandlordModel.findById(decoded.userID)
 
     if (!landlord) {
@@ -29,10 +33,15 @@ export const stripeSubscriptionCheckoutFormController = async (req: Request, res
       return res.status(500).json({ message: 'Stripe key missing!' })
     }
 
-    const stripeAccess = new Stripe(STRIPE_TEST_SECRET_KEY)
+    // Instantiating a new Stripe object for stripe interactions
+    const stripeAccess = new Stripe(STRIPE_TEST_SECRET_KEY, {
+      apiVersion: '2025-09-30.clover',
+    })
 
+    // If the user already has a stripe customer id save it here
     let stripeCustomerId = landlord.stripeCustomerId;
 
+    // If the user doesn't have the stripe customer id create it here and update it in the database
     if (!stripeCustomerId) {
       const customer = await stripeAccess.customers.create({
         email: landlord.email,
@@ -41,11 +50,12 @@ export const stripeSubscriptionCheckoutFormController = async (req: Request, res
       stripeCustomerId = customer.id;
 
       await LandlordModel.updateOne(
-        { _id: landlord._id},
+        { _id: landlord._id },
         { stripeCustomerId }
       )
     }
 
+    // This is the checkout flow when a user is paying for a subscription.
     const session = await stripeAccess.checkout.sessions.create({
       payment_method_types: ["card"],
       mode: "subscription",
