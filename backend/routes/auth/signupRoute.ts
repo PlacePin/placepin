fcdf9b246690121600d8115fd31b165ed70c17b8
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-10-26 12:22:37 -0400

    deleted login and signup routes

--- backend/routes/auth/signupRoute.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { signupController } from '../../controllers/auth/signupController';
-
-const router = express.Router()
-
-router.post('/signup', signupController)
-
-export default router
