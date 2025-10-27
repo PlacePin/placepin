7e2cb7a47575b2c4a9c94ded3374e1d0691d667c
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-10-27 12:32:40 -0400

    remove unneeded imports

--- backend/controllers/resources/userController.ts
+++ backend/controllers/resources/userController.ts
@@ -1,14 +1,10 @@
 import type { Request, Response } from "express";
-import jwt from "jsonwebtoken";
-import dotenv from 'dotenv';
 import { getUserById } from "../../utils/user";
-
-dotenv.config();
 
 export const userController = async (req: Request, res: Response) => {
   const userId = req.userId
 
-  if(!userId) {
+  if (!userId) {
     return res.status(401).json({ message: "Invalid token" });
   }
 
@@ -21,10 +17,6 @@ export const userController = async (req: Request, res: Response) => {
 
     return res.status(200).json({ user })
   } catch (err) {
-    if (err instanceof jwt.JsonWebTokenError) {
-      return res.status(400).json({ message: err.message });
-    } else {
-      return res.status(500).json({ message: 'Oops! Something went wrong looking for a subscription tier.' })
-    }
+    return res.status(500).json({ message: 'Oops! Something went wrong looking for a subscription tier.' })
   }
 }
