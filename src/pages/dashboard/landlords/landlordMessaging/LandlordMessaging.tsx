00783a1ffd17d4d10151fc8c99e3d0fc25ceb04f
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-10-28 13:57:32 -0400

    messaging page

--- src/pages/dashboard/landlords/landlordMessaging/LandlordMessaging.tsx
+++ src/pages/dashboard/landlords/landlordMessaging/LandlordMessaging.tsx
@@ -1,11 +1,35 @@
+import { useState } from 'react';
 import styles from './landlordMessaging.module.css'
 
 const LandlordMessaging = () => {
+
+  const [activeTab, setActiveTab] = useState<string[]>([]);
+
   return (
-    <div className={styles.container}>
-      <h2 className={styles.noData}>No Data</h2>
-      <div className={styles.noDataButtonContainer}>
-        <button className={styles.button}>Start a message</button>
+    <div>
+      <h2 className={styles.title}>Messages</h2>
+      <div className={styles.wrapper}>
+        <div
+          className={styles.messagesContainer}
+        >
+          <p
+          className={styles.startMessage}
+          >
+            Compose
+            </p>
+          {/* <div className={styles.noDataButtonContainer}>
+          <button className={styles.button}>Start a message</button>
+        </div> */}
+        </div>
+        <div
+          className={styles.compose}
+        >
+        </div>
+        <div
+        className={styles.promo}
+        >
+
+        </div>
       </div>
     </div>
   )
