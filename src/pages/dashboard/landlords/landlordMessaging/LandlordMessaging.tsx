b763965a06c15f1b8196c930baf87ec6d14bd719
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-10-28 15:26:28 -0400

    active states add

--- src/pages/dashboard/landlords/landlordMessaging/LandlordMessaging.tsx
+++ src/pages/dashboard/landlords/landlordMessaging/LandlordMessaging.tsx
@@ -1,32 +1,51 @@
 import { useState } from 'react';
 import styles from './landlordMessaging.module.css'
+import { Plus } from 'lucide-react';
 
 const LandlordMessaging = () => {
 
-  const [activeTab, setActiveTab] = useState<string[]>([]);
+  const [activeMessage, setActiveMessage] = useState<string[]>(["Isabella", "Calvin", "Kenji", "Ralph", "Aaron", "Yves", "Marcaine", "Mirthaud", "Caliyah"]);
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+  const people = activeMessage.map((person, i) => {
+    return (
+      <p
+        key={i}
+        className={`${styles.person} ${activeIndex === i ? styles.activeMessage : ""}`}
+        onClick={() => setActiveIndex(i)}
+      >
+        {person}
+      </p>
+    )
+  });
 
   return (
     <div>
       <h2 className={styles.title}>Messages</h2>
       <div className={styles.wrapper}>
-        <div
-          className={styles.messagesContainer}
-        >
-          <p
-          className={styles.startMessage}
+        <div className={styles.leftContainer}>
+          <div
+            className={styles.composeContainer}
           >
-            Compose
+            <p
+              className={styles.compose}
+            >
+              <Plus />Compose
             </p>
-          {/* <div className={styles.noDataButtonContainer}>
-          <button className={styles.button}>Start a message</button>
-        </div> */}
+          </div>
+          <div
+            className={styles.messagesList}
+          >
+            <p>{people}</p>
+          </div>
         </div>
         <div
-          className={styles.compose}
+          className={styles.convo}
         >
+
         </div>
         <div
-        className={styles.promo}
+          className={styles.promo}
         >
 
         </div>
