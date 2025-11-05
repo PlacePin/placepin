c11d4dc0f6e9b436e437407a7e01702ef888273e
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-11-04 19:12:23 -0500

    tenant insights under construction

--- src/pages/dashboard/landlords/landlordTenantInsights/LandlordTenantInsights.tsx
+++ src/pages/dashboard/landlords/landlordTenantInsights/LandlordTenantInsights.tsx
@@ -1,28 +1,55 @@
 import { useState } from 'react';
 import styles from './landlordTenantInsights.module.css'
 import InviteTenantModal from '../../../../components/modals/InviteTenantModal';
+import { useGetAxios } from '../../../../hooks/useGetAxios';
 
 const LandlordTenantInsights = () => {
 
   const [showInviteModal, setShowInviteModal] = useState(false);
 
+  const { data, error } = useGetAxios(`/api/landlords/tenants`);
+
+  // Todo: Fix this so skeleton loading or cache so null doesn't render on each re-render
+  if (!data) {
+    return <div></div>;
+  }
+
+  if (error) {
+    return <div>{"Something went wrong, but don't panic, we'll fix it!"}</div>
+  }
+
+  const tenants = data.tenants
+
+  console.log(tenants)
+
   return (
-    <div className={styles.container}>
-      <h2 className={styles.noData}>No Data</h2>
-      <div className={styles.noDataButtonContainer}>
-        <button
-          className={styles.button}
-          onClick={() => setShowInviteModal(prev => !prev)}
-        >
-          Invite Tenants
-        </button>
-      </div>
-      {showInviteModal && (
-        <InviteTenantModal
-          onClose={() => setShowInviteModal(prev => !prev)}
-        />
-      )}
-    </div>
+    <>
+      {tenants.length
+        ?
+        <div className={styles.container}>
+          <h2>Tenant Insights</h2>
+          <div>
+          </div>
+        </div>
+        :
+        <div>
+          <h2 className={styles.noData}>No Data</h2>
+          <div className={styles.noDataButtonContainer}>
+            <button
+              className={styles.button}
+              onClick={() => setShowInviteModal(prev => !prev)}
+            >
+              Invite Tenants
+            </button>
+          </div>
+          {showInviteModal && (
+            <InviteTenantModal
+              onClose={() => setShowInviteModal(prev => !prev)}
+            />
+          )}
+        </div>
+      }
+    </>
   )
 }
 
