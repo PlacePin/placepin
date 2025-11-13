5f5edfc644e3ea666280d9d3a0a613978907f54f
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-11-13 14:14:33 -0500

    fixed tenant summary layout

--- src/pages/dashboard/landlords/landlordTenants/tenantPortal/Tenantsummary.tsx
+++ src/pages/dashboard/landlords/landlordTenants/tenantPortal/Tenantsummary.tsx
@@ -2,11 +2,11 @@ import styles from './tenantSummary.module.css';
 
 const TenantSummary = () => {
   return (
-    <div>
+    <div className={styles.summaryWrapper}>
       <div className={styles.photoSection}>
         Pic section
       </div>
-      <div>
+      <div className={styles.tenantDetailsWrapper}>
         <div className={styles.tenantDetails}>
           <div className={styles.daysAsTenant}>Days as Tenant</div>
           <div className={styles.expenses}>Expenses</div>
