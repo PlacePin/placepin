6b39204573b7ee7c3ca4ffb7952524b067fd45ed
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-11-15 12:23:32 -0500

    tenant activity complete

--- src/pages/dashboard/landlords/landlordTenants/tenantPortal/TenantActivity.tsx
+++ src/pages/dashboard/landlords/landlordTenants/tenantPortal/TenantActivity.tsx
@@ -1,4 +1,30 @@
 import styles from './tenantActivity.module.css';
+import {
+  Chart as ChartJS,
+  RadialLinearScale,
+  ArcElement,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { PolarArea } from "react-chartjs-2";
+
+ChartJS.register(RadialLinearScale, ArcElement, Tooltip, Legend);
+
+const data = {
+  labels: ["Electrician", "Plumbing", "Carpentry", "Misc."],
+  datasets: [
+    {
+      label: "Tradesmen",
+      data: [11, 16, 7, 3],
+      backgroundColor: [
+        "#00bfa5",
+        "#0F5FC2",
+        "#FFCF56",
+        "#DA001A",
+      ],
+    },
+  ],
+};
 
 const TenantActivity = () => {
 
@@ -26,11 +52,16 @@ const TenantActivity = () => {
     <div className={styles.activityWrapper}>
       <div className={styles.paymentMaintenance}>
         <div className={`${styles.defaultCardStyles} ${styles.payment}`}>
-          <p className={styles.paymentHistory}>Payment History</p>
+          <p className={styles.title}>Payment History</p>
           {rentPaymentsMapped}
         </div>
         <div className={`${styles.defaultCardStyles} ${styles.maintenance}`}>
-          Maintenance Request
+          <p className={styles.title}>Maintenance Request</p>
+          <div className={styles.chartSection}>
+            <PolarArea
+              data={data}
+            />
+          </div>
         </div>
       </div>
       <div className={`${styles.defaultCardStyles} ${styles.engagement}`}>
