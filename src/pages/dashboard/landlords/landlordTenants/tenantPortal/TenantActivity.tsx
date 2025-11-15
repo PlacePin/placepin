c5051e40aec5d11c073b73124433425af035565c
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-11-15 02:39:37 -0500

    payment section implemented

--- src/pages/dashboard/landlords/landlordTenants/tenantPortal/TenantActivity.tsx
+++ src/pages/dashboard/landlords/landlordTenants/tenantPortal/TenantActivity.tsx
@@ -1,14 +1,36 @@
 import styles from './tenantActivity.module.css';
 
 const TenantActivity = () => {
+
+  const rentPayments = [
+    { rent: 3000, paid: '11/1/25' },
+    { rent: 3000, paid: '10/1/25' },
+    { rent: 3000, paid: '9/1/25' },
+    { rent: 3000, paid: '8/1/25' },
+    { rent: 3000, paid: '7/1/25' },
+    { rent: 3000, paid: '6/1/25' },
+    { rent: 3000, paid: '5/1/25' },
+  ]
+
+  const rentPaymentsMapped = rentPayments.map((rentPayment, i) => {
+    return (
+      <div key={i} className={styles.rentPayment}>
+        <p>
+          <span className={styles.rentAmount}>{rentPayment.rent}</span> a month Paid On: <span className={styles.date}>{rentPayment.paid}</span>
+        </p>
+      </div>
+    )
+  })
+
   return (
     <div className={styles.activityWrapper}>
       <div className={styles.paymentMaintenance}>
         <div className={`${styles.defaultCardStyles} ${styles.payment}`}>
-          Payment History
+          <p className={styles.paymentHistory}>Payment History</p>
+          {rentPaymentsMapped}
         </div>
         <div className={`${styles.defaultCardStyles} ${styles.maintenance}`}>
-          Maintenance Request 
+          Maintenance Request
         </div>
       </div>
       <div className={`${styles.defaultCardStyles} ${styles.engagement}`}>
