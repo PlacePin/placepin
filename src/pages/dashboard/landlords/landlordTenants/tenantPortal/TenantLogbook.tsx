12fab9f0b2ba463b43f9e8023b2cde8cc636f722
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-11-15 22:15:45 -0500

    constructing logbook

--- src/pages/dashboard/landlords/landlordTenants/tenantPortal/TenantLogbook.tsx
+++ src/pages/dashboard/landlords/landlordTenants/tenantPortal/TenantLogbook.tsx
@@ -3,7 +3,16 @@ import styles from './tenantLogbook.module.css';
 const TenantLogbook = () => {
   return (
     <div className={styles.logbookWrapper}>
-      Logbook
+      <p className={styles.title}>Logbook</p>
+      <div className={styles.infoContainer}>
+        <p>Landlord References: <span>{'They were great!'}</span></p>
+        <p>Income: <span>{'$95,000'}</span></p>
+        <p>Job: <span>{'Software Engineer'}</span></p>
+        <p>Age: <span>{27}</span></p>
+        <p>Section 8: <span>{'Yes'}</span></p>
+        <p>Government Assistants: <span>{'Yes'}</span></p>
+        <p>Disability: <span>{'True'}</span></p>
+      </div>
     </div>
   )
 }
