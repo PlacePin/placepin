4db59870555f9c15913851b845a28cb0c503d392
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-11-22 14:21:06 -0500

    added a placeholder image to the house portal

--- src/pages/dashboard/landlords/landlordProperties/propertyPortal/PropertySummary.tsx
+++ src/pages/dashboard/landlords/landlordProperties/propertyPortal/PropertySummary.tsx
@@ -3,7 +3,13 @@ import styles from './propertySummary.module.css';
 const PropertySummary = () => {
   return (
     <section className={styles.summaryWrapper}>
-      <div>img</div>
+      <div>
+        <img 
+        src='/townhouse.png'
+        alt='housing picture'
+        className={styles.image}
+        />
+        </div>
       <div>stats</div>
       <div>Work Orders</div>
       <div>retainers</div>
