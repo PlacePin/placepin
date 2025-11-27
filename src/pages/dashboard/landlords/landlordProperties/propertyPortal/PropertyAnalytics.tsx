6bb4e9d6ea9d4067a658d2afe2961274131fc523
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-11-27 02:42:32 -0500

    constructed property analytics

--- src/pages/dashboard/landlords/landlordProperties/propertyPortal/PropertyAnalytics.tsx
+++ src/pages/dashboard/landlords/landlordProperties/propertyPortal/PropertyAnalytics.tsx
@@ -3,21 +3,57 @@ import styles from './propertyAnalytics.module.css';
 
 const PropertyAnalytics = () => {
   return (
-    <div>
-      <Tooltip
-        text={'This is an estimate.'}
-        position={'top'}
-      >
-        Outstanding Principal
-      </Tooltip>
-      <div>Mortgage</div>
-      <div>Interest Rate</div>
-      <Tooltip
-        text={'This is an estimate.'}
-        position={'top'}
-      >
-        Projected Equity
-      </Tooltip>
+    <div className={styles.analyticsContainer}>
+      <div className={styles.header}>
+        <h4>
+          Financials
+        </h4>
+      </div>
+      <div className={styles.analyticsWrapper}>
+        <div className={styles.sectionContainers}>
+          <span>
+            {'$800,000'}
+          </span>
+          <Tooltip
+            text={'This is an estimate.'}
+            position={'top'}
+          >
+            <p>Outstanding Principal</p>
+          </Tooltip>
+        </div>
+        <div className={styles.sectionContainers}>
+          <span>
+            {'$4,000'}
+          </span>
+          <Tooltip
+            text={'This is an estimate.'}
+            position={'top'}
+          >
+            <p>Mortgage</p>
+          </Tooltip>
+        </div>
+        <div className={styles.sectionContainers}>
+          <span>
+            {'3.75%'}
+          </span>
+          <p>
+            Interest Rate
+          </p>
+        </div>
+        <div className={styles.sectionContainers}>
+          <span>
+            {'$10,000'}
+          </span>
+          <Tooltip
+            text={'This is an estimate.'}
+            position={'top'}
+          >
+            <p>
+              Projected Equity
+            </p>
+          </Tooltip>
+        </div>
+      </div>
     </div>
   );
 };
