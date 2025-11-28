71e1b778ce9940ba4f521e51f94cc73d312830d8
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-11-28 09:47:44 -0500

    added property details details

--- src/pages/dashboard/landlords/landlordProperties/propertyPortal/PropertyDetails.tsx
+++ src/pages/dashboard/landlords/landlordProperties/propertyPortal/PropertyDetails.tsx
@@ -3,7 +3,12 @@ import styles from './propertyDetails.module.css';
 const PropertyDetails = () => {
   return (
     <div className={styles.detailsWrapper}>
-      Details
+      <div>Square Feet</div>
+      <div>Trash Pickup</div>
+      <div>Electrician Last Updated</div>
+      <div>Boiler Last Updated</div>
+      <div>Closest Public Commutes</div>
+      <div>Average Unit Sizes</div>
     </div>
   )
 }
