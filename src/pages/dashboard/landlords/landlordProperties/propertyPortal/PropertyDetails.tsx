f95f75b6c2874bb0634d2badb92ed8e06aa64b88
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-11-29 12:32:13 -0500

    property details constructed

--- src/pages/dashboard/landlords/landlordProperties/propertyPortal/PropertyDetails.tsx
+++ src/pages/dashboard/landlords/landlordProperties/propertyPortal/PropertyDetails.tsx
@@ -2,13 +2,50 @@ import styles from './propertyDetails.module.css';
 
 const PropertyDetails = () => {
   return (
-    <div className={styles.detailsWrapper}>
-      <div>Square Feet</div>
-      <div>Trash Pickup</div>
-      <div>Electrician Last Updated</div>
-      <div>Boiler Last Updated</div>
-      <div>Closest Public Commutes</div>
-      <div>Average Unit Sizes</div>
+    <div className={styles.detailsContainer}>
+      <div className={styles.header}>
+        <h4>
+          Property Details
+        </h4>
+      </div>
+      <div className={styles.detailsWrapper}>
+        <div className={styles.sectionContainers}>
+          <span className={styles.values}>
+            {'4000'}
+          </span>
+          <p>Square Feet</p>
+        </div>
+        <div className={styles.sectionContainers}>
+          <span className={styles.values}>
+            {'Wednesday'}
+          </span>
+          <p>Trash Pickup</p>
+        </div>
+        <div className={styles.sectionContainers}>
+          <span className={styles.values}>
+            {'October 2022'}
+          </span>
+          <p>Electrician Last Updated</p>
+        </div>
+        <div className={styles.sectionContainers}>
+          <span className={styles.values}>
+            {'N/A'}
+          </span>
+          <p>Boiler Last Updated</p>
+        </div>
+        <div className={styles.sectionContainers}>
+          <span className={styles.values}>
+            {'Bus - 1 min walk'}
+          </span>
+          <p>Closest Public Commutes</p>
+        </div>
+        <div className={styles.sectionContainers}>
+          <span className={styles.values}>
+            {'500 SQF'}
+          </span>
+          <p>Average Unit Sizes</p>
+        </div>
+      </div>
     </div>
   )
 }
