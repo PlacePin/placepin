d091d7b072e561bed3dd9faba49d7930b84f0883
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-12-08 18:09:07 -0500

    constructing property model

--- backend/database/models/Property.model.ts
+++ backend/database/models/Property.model.ts
@@ -3,7 +3,27 @@ import mongoose, { type InferSchemaType, type HydratedDocument } from 'mongoose'
 // This is the shape of the property collection object
 
 const propertySchema = new mongoose.Schema({
-
+address: {
+    number: String,
+    street: String,
+    streetType: String,
+    unit: { type: String, default: undefined },
+    city: String,
+    state: String,
+    zip: String
+  },
+  taxYear: {
+    year: {
+      year: Number,
+      receipts: {
+        expenseCategory: String,
+        amount: Number,
+        date: Date,
+        paymentMethod: String,
+        description: String,
+      }
+    }
+  }
 });
 
 // This is the type as a js object
