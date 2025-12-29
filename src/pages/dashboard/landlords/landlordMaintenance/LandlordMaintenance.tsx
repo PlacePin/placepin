d1cca9bbad183a59bf540f2eadc1b8f45b1b4e67
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-12-29 17:29:29 -0500

    created tradesmen card skeleton

--- src/pages/dashboard/landlords/landlordMaintenance/LandlordMaintenance.tsx
+++ src/pages/dashboard/landlords/landlordMaintenance/LandlordMaintenance.tsx
@@ -1,6 +1,37 @@
+import { Info, User } from 'lucide-react';
 import styles from './landlordMaintenance.module.css';
+import { capitalizeWords } from '../../../../utils/stringUtils';
 
 const LandlordMaintenance = () => {
+
+  const tradesmenCards = <div
+        // key={tenant._id}
+        className={styles.tenantCards}
+      >
+        <User
+          width={150}
+          height={200}
+          strokeWidth={1}
+        />
+        <div
+          className={styles.descriptionWrapper}
+        >
+          <p>
+            {/* {capitalizeWords(tenant.fullName)} */}
+          </p>
+          <button
+            className={styles.infoButton}
+            // onClick={() => setSelectedTenant(tenant)}
+          >
+            <Info
+              size={18}
+              className={styles.infoIcon}
+            />
+            Info
+          </button>
+        </div>
+      </div>
+
   return (
     <div className={styles.container}>
       <h2 className={styles.noData}>Hire a Tradesman</h2>
