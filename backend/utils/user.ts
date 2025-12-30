af1e2ec5c4ce0027a8a157dfa0ec0c61eee2b2ed
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-12-29 19:03:08 -0500

    added tradesmen to user util function

--- backend/utils/user.ts
+++ backend/utils/user.ts
@@ -1,11 +1,12 @@
 import { LandlordModel } from "../database/models/Landlord.model";
 import { TenantModel } from "../database/models/Tenant.model";
+import { TradesmenModel } from "../database/models/Tradesmen.model";
 
 export const excludeFields = '-password -__v -subscription.savedPaymentMethod -subscription.stripeCustomerId'
 
 export const getUserById = async (userID: string, excludeFields: string) => {
 
   return (await TenantModel.findById(userID).select(excludeFields)) || 
-  (await LandlordModel.findById(userID).select(excludeFields));
+  (await LandlordModel.findById(userID).select(excludeFields)) ||
+  (await TradesmenModel.findById(userID).select(excludeFields));
 };
-
