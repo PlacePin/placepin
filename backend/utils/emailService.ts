4dcc24c65c5af6dbab9a71ded02f23cbd97e312c
LDER-DEV <ludjy.dev@gmail.com>
2026-03-20 16:04:06 -0400

    added emailPasswordReset function to email services utility  and created frogotten password template

--- backend/utils/emailService.ts
+++ backend/utils/emailService.ts
@@ -50,3 +50,55 @@ export const emailInviteToTenant = async (referralCode: string, tenantName: stri
 
 
 }
+
+export const emailPasswordReset = async (userEmail: string, resetUrl: string) => {
+
+  // const isSandbox = true;
+
+  // const MAILTRAP_TEST_USERNAME = process.env.MAILTRAP_TEST_USERNAME!
+  // const MAILTRAP_TEST_PASSWORD = process.env.MAILTRAP_TEST_PASSWORD!
+  // const MAILTRAP_SMTP_SANDBOX = process.env.MAILTRAP_SMTP_SANDBOX!
+  const GMAIL_USER = process.env.GMAIL_USER!
+  const GMAIL_APP_PASSWORD = process.env.GMAIL_APP_PASSWORD!
+  const GMAIL_SMTP = process.env.GMAIL_SMTP!
+
+  // Switch to real env's for productions
+  // const transporter = nodemailer.createTransport({
+  //   host: isSandbox ? MAILTRAP_SMTP_SANDBOX : process.env.MAILTRAP_SMTP,
+  //   port: 587,
+  //   secure: false,
+  //   auth: {
+  //     user: isSandbox ? MAILTRAP_TEST_USERNAME : process.env.MAILTRAP_USERNAME,
+  //     pass: isSandbox ? MAILTRAP_TEST_PASSWORD : process.env.MAILTRAP_PASSWORD,
+  //   },
+  // });
+
+  // This gmail version works fine but at scale switch over to MailTrap
+
+  const transporter = nodemailer.createTransport({
+    host: GMAIL_SMTP,
+    port: 465,
+    secure: true,
+    auth: {
+      user: GMAIL_USER,
+      pass: GMAIL_APP_PASSWORD,
+    },
+  });
+
+  const info = await transporter.sendMail({
+    // Currently sending out email reset from personal gmail account, switch to company account when live
+    from: GMAIL_USER,
+    to: `${userEmail}`,
+    subject: "PlacePin Password Reset Request",
+    text: `<h2>Reset your password</h2>
+    <p>'We recieved a request to reset your password for your PlacePin account.'</p>
+    <p> Please click the link below to choose a new password. This link will expire in 1 hour </p>
+    <a href = '${resetUrl}'> Reset Password </a>
+    `,
+  });
+
+  // If you want to see the message object uncomment!
+  console.log("Message sent:", info);
+
+
+}
