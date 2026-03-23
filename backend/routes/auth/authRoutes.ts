3e4ee30c76cdbbdf616f28d2a2022ee8301a862a
LDER-DEV <ludjy.dev@gmail.com>
2026-03-23 16:22:40 -0400

    created the resetPasswordController and routes for the forgot password and reset password controllers

--- backend/routes/auth/authRoutes.ts
+++ backend/routes/auth/authRoutes.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import { loginController } from '../../controllers/auth/loginController';
 import { signupController } from '../../controllers/auth/signupController';
+import { forgotPasswordController } from '../../controllers/auth/forgotPasswordController';
+import { resetPasswordController } from '../../controllers/auth/resetPasswordController';
 
 const router = express.Router();
 
 router.post('/login', loginController);
 router.post('/signup', signupController);
+router.post('/forgot-password', forgotPasswordController);
+router.post('/reset-password/:token', resetPasswordController);
 
 export default router
