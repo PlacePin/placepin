c563b6714a5186fda6eecadded61dad8afcf9c6d
Kerlin Augustin <kerlin.augustin1@gmail.com>
2026-04-14 23:47:21 -0400

    added cron job

--- backend/jobs/index.ts
+++ backend/jobs/index.ts
@@ -1,14 +1,15 @@
 import cron from 'node-cron';
+import { LandlordModel } from '../database/models/Landlord.model';
 import { snapshotFinancials } from './snapshotFinancials';
 import { processRentPayments } from './processRentPayments';
-import { LandlordModel } from '../database/models/Landlord.model';
+import { resetRentStatus } from './resetRentStatus';
 
 // Runs at midnight on the 1st of every month
 cron.schedule('0 0 1 * *', async () => {
   console.log('Running monthly financial snapshot...');
 
   const landlords = await LandlordModel.find({}, '_id');
-  
+
   for (const landlord of landlords) {
     await snapshotFinancials(landlord._id.toString());
   }
@@ -24,4 +25,8 @@ cron.schedule('0 1 1 * *', async () => {
 // Charge tenants whose rent is due on the 15th
 cron.schedule('0 1 15 * *', async () => {
   await processRentPayments(15);
+});
+
+cron.schedule('0 1 26 * *', async () => {
+  await resetRentStatus();
 });
