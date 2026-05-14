a56c4f03f765ea830c39e0468c718f199e355b05
Kerlin Augustin <kerlin.augustin1@gmail.com>
2026-05-14 14:43:28 -0400

    setting up tenant paying rent

--- src/pages/dashboard/tenants/tenantHomepage/TenantHomepage.tsx
+++ src/pages/dashboard/tenants/tenantHomepage/TenantHomepage.tsx
@@ -1,11 +1,21 @@
+import StatsKPICard from '../../../../components/cards/landlord/StatsKPICard';
 import styles from './tenantHompage.module.css';
 
 const TenantHomepage = () => {
   return (
-    <div>
-      <h2 className={styles.title}>
+    <div className={styles.landlordHomepageContainer}>
+      <h2>
         Upcoming / Recent Activity
       </h2>
+      <div className={styles.statsCards}>
+        <StatsKPICard
+          title={'Rent Due'}
+          ctaText={'Pay Rent'}
+          handleClick={() => { }}
+        >
+          ''
+        </StatsKPICard>
+      </div>
     </div>
   )
 }
